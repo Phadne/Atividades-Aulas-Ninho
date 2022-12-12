888be526445b9ce8cc3ecf16a38c5f0c959dcd42
Daphne Torrelio <daphnetorrelio365@gmail.com>
2022-12-12 09:02:44 -0300

    Create Hello world.py

---
+++ Hello world.py
@@ -0,0 +1,8 @@
+a = int(input("digite o valor "))
+b = int(input("digite o valor de b"))
+c = int(input("digite o valor de c"))
+
+if (a+b) < c: 
+    print("o valor de a + b é menor que c")
+else:
+    print("o valor de a + b é maior que c")
