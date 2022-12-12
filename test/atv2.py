8a1efd5e698c9e8269b94586d4f811c094e0f8d6
Daphne Torrelio <daphnetorrelio365@gmail.com>
2022-12-12 09:01:42 -0300

    Create atv2.py

---
+++ test/atv2.py
@@ -0,0 +1,23 @@
+def helloworld():
+    print("Hello World")
+
+def hello(meu_nome):
+    print("Hello", meu_nome)
+
+helloworld()
+
+helloworld(input("Digite seu nome:"))
+
+nome = (input('Digite seu nome:'))
+hello(nome)
+
+
+def hello(meu_nome,minha_idade):
+    print "Olá",meu_nome,"\ Sua idade é : ",minha_idade")
+
+helloworld()
+
+nome = (input("Digite seu nome: "))
+idade = (input("Digite a sua idades: "))
+
+ hello(nome, idade)
