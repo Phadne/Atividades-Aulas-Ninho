95ed68d0d2fc20ca5c863a7533b1fcabd7ef4bdd
Daphne Torrelio <daphnetorrelio365@gmail.com>
2022-12-12 09:00:24 -0300

    Create tst2.py

---
+++ tst2.py
@@ -0,0 +1,47 @@
+#def f5 (a):
+    #a = [10,10]
+    #a.append(3)
+    #print(a)
+    #print("Dentro da função;",a)
+
+#a = [1,2]    
+#f5(a)
+#print("Fora da função")
+
+# palavra = input("insira uma palavra")
+
+# while = (True):
+
+#     letra = input("insira o caractere")
+#     if (len(letra) == 1):
+#         break
+#     else:
+#         print("try again!")
+
+# print("o caractere '{letra}' na '{palavra}' apareceu '{checagem}' vezes")        
+
+    # def checarLetra(p,l):
+    #  contador = 0
+
+    #  for i in range(len(p)):
+#         if(p[i] == l):
+#             contador += 1
+#     return contador
+
+# palavra = input("digite uma palavra:")
+# letra = input("digite uma letra:")
+
+  print(checarLetra(palavra,letra))
+
+    def checagemsenha(senha):
+        if (len(senha)>= 4 and len(senha)<=8)
+            if senha.isnumeric():
+             global repetir
+             repetir=False
+                print("senha inválida")
+    else:
+      senha=input("insira uma senha entre 4 e 8 digitos, que utilize apenas números:")
+
+    if(senha.isnumeric):
+        if(len(senha)>=4 and len(senha)<=8):
+            print('senha')
